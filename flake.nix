b2d7055348acaf600c6ff3a863fbaf362f9e3f45
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-07-26 15:09:56 +0000

    include frontend

--- flake.nix
+++ flake.nix
@@ -16,78 +16,56 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        streamlit-cookies-controller-npm-built = pkgs.stdenv.mkDerivation rec {
-          name = "streamlit-cookies-controller-src";
-          src = pkgs.fetchFromGitHub {
-            owner = "NathanChen198";
-            repo = "streamlit-cookies-controller";
-            rev = "949d52732b95250ffbebef2181d591ea9a4fbdcc";
-            sha256 = "sha256-uJk6xKDQ6ACCJ38wTlpe4HtPdECFnkIvLzRJKWnrpqo=";
-          };
-
-          installPhase =
-            let
-              frontend-build =
-                let
-                  git_src = src;
-                in
-                pkgs.buildNpmPackage rec {
-                  name = "st-cookie-controller-frontend";
-                  pname = "st-cookie-controller-frontend";
-                  src = "${git_src}/streamlit_cookies_controller/frontend";
-                  npmDepsHash = "sha256-zbWpVSYi873lU84J1SG+Zf98Z+ldpA5wAeC8utLtj6s=";
-                  NODE_OPTIONS = "--openssl-legacy-provider";
-                };
-              
-            in
-            ''
-              mkdir -p $out/streamlit_cookies_controller/frontend
-              cp $src/streamlit_cookies_controller/*.py $out/streamlit_cookies_controller/
-              cp $src/*.py $out/
-              cp $src/README.md $out/
-              cp $src/MANIFEST.in $out/
-              cp -r ${frontend-build} $out/streamlit_cookies_controller/frontend
-            '';
-
-        };
       in
       {
         packages = {
 
-  streamlit-cookies-controller-npm-built = pkgs.stdenv.mkDerivation rec {
-          name = "streamlit-cookies-controller-src";
-          src = pkgs.fetchFromGitHub {
-            owner = "NathanChen198";
-            repo = "streamlit-cookies-controller";
-            rev = "949d52732b95250ffbebef2181d591ea9a4fbdcc";
-            sha256 = "sha256-uJk6xKDQ6ACCJ38wTlpe4HtPdECFnkIvLzRJKWnrpqo=";
-          };
-
-          installPhase =
+          streamlit-cookies-controller =
             let
-              frontend-build =
-                let
-                  git_src = src;
-                in
-                pkgs.buildNpmPackage rec {
-                  name = "st-cookie-controller-frontend";
-                  pname = "st-cookie-controller-frontend";
-                  src = "${git_src}/streamlit_cookies_controller/frontend";
-                  npmDepsHash = "sha256-zbWpVSYi873lU84J1SG+Zf98Z+ldpA5wAeC8utLtj6s=";
-                  NODE_OPTIONS = "--openssl-legacy-provider";
-                };
-              
+              git_src = pkgs.fetchFromGitHub {
+                owner = "NathanChen198";
+                repo = "streamlit-cookies-controller";
+                rev = "949d52732b95250ffbebef2181d591ea9a4fbdcc";
+                sha256 = "sha256-uJk6xKDQ6ACCJ38wTlpe4HtPdECFnkIvLzRJKWnrpqo=";
+              };
             in
-            ''
-              mkdir -p $out/streamlit_cookies_controller/frontend
-              cp $src/streamlit_cookies_controller/*.py $out/streamlit_cookies_controller/
-              cp $src/*.py $out/
-              cp $src/README.md $out/
-              cp $src/MANIFEST.in $out/
-              cp -r ${frontend-build}/lib/node_modules/cookie_controller/build $out/streamlit_cookies_controller/frontend
-            '';
+            pkgs.stdenv.mkDerivation rec {
+              name = "streamlit-cookies-controller";
+              src = pkgs.python311Packages.buildPythonPackage rec {
+                pname = "streamlit-cookies-controller";
+                version = "0.1.71";
 
-        };
+                propagatedBuildInputs = [ pkgs.python311Packages.streamlit ];
+
+                src = git_src;
+                #doCheck = false; # nicht meine schuld dass deren tests failen
+                meta = {
+                  description = "Control client browser cookie for the site";
+                  homepage = "https://github.com/NathanChen198/streamlit-cookies-controller";
+                  license = pkgs.lib.licenses.mit;
+                };
+              };
+              installPhase =
+                let
+                  frontend-build =
+
+                    pkgs.buildNpmPackage rec {
+                      name = "st-cookie-controller-frontend";
+                      pname = "st-cookie-controller-frontend";
+                      src = "${git_src}/streamlit_cookies_controller/frontend";
+                      npmDepsHash = "sha256-zbWpVSYi873lU84J1SG+Zf98Z+ldpA5wAeC8utLtj6s=";
+                      NODE_OPTIONS = "--openssl-legacy-provider";
+                    };
+
+                in
+                ''
+                  mkdir -p $out/lib/python3.11/site-packages/streamlit_cookies_controller/frontend
+                  cp -r $src/lib $out
+                  cp -r $src/nix-support $out
+                  cp -r ${frontend-build}/lib/node_modules/cookie_controller/build $out/lib/python3.11/site-packages/streamlit_cookies_controller/frontend
+                '';
+
+            };
 
           fsrs = pkgs.python311Packages.buildPythonPackage rec {
             pname = "fsrs";
@@ -108,20 +86,7 @@
               license = pkgs.lib.licenses.mit;
             };
           };
-          streamlit-cookies-controller = pkgs.python311Packages.buildPythonPackage rec {
-            pname = "streamlit-cookies-controller";
-            version = "0.1.71";
 
-            propagatedBuildInputs = [ pkgs.python311Packages.streamlit ];
-
-            src = streamlit-cookies-controller-npm-built;
-            #doCheck = false; # nicht meine schuld dass deren tests failen
-            meta = {
-              description = "Control client browser cookie for the site";
-              homepage = "https://github.com/NathanChen198/streamlit-cookies-controller";
-              license = pkgs.lib.licenses.mit;
-            };
-          };
         };
       }
     );
